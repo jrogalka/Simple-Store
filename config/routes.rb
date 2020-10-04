74a96c1358e7579dfb9a356965f8fe4b6c32866b
Joel Rogalka <jrogalka@outlook.com>
2020-10-03 20:54:14 -0500

    Added products view and updated to RESTful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :products, only: %i[index show]
 end
